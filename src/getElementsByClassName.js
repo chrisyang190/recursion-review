0be2296938a999e686958a823c3c3bc15423f1fe
chrisyang190 <chrisyang190@gmail.com>
2017-02-06 13:27:11 -0800

    Complete stringifyJSON & getElementsbyClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var result = [];
+
+  var body = document.body;
+
+  var walkDom = function(node){
+    // base case
+    if (node.classList && node.classList.contains(className)) {
+      result.push(node);
+    }
+    // recursive case
+    node = node.firstChild
+    while (node) {
+      //node = node.firstChild;
+      walkDom(node);
+      node = node.nextSibling;
+    }
+
+
+  };
+  walkDom(body);
+
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,38 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  // primitive conditions
+
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return '' + obj + '';
+  //strings
+  } else if (typeof obj === 'string'){
+    return '\"' + obj + '\"';
+  }
+
+
+  //arrays
+  else if (Array.isArray(obj)){
+    var results = [];
+    obj.forEach(function(s){
+      results.push(stringifyJSON(s));
+    });
+
+    return '[' + results + ']';
+  }
+
+
+  //objects
+  else if (typeof obj === 'object') {
+    var result = [];
+    for (var key in obj) { 
+      if (typeof obj[key] === 'function' || obj[key] === undefined){
+        continue;
+      }
+      result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    } // [key: value1, key2: value2]
+
+    return '{' + result.join(',') + '}';
+  }
 };
